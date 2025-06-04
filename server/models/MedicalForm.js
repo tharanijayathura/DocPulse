debc9984b9f2faa4b6640b2e2e7eaa50a4894ab0
Tharani Jayathura <tharanijayathura1@gmail.com>
2025-06-05 01:50:08 +0530

    removed old file stuff

--- server/models/MedicalForm.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const medicalFormSchema = new mongoose.Schema({
-  patientName: { type: String, required: true },
-  symptoms: { type: String, required: true },
-  notes: { type: String }
-});
-
-module.exports = mongoose.model('MedicalForm', medicalFormSchema);
